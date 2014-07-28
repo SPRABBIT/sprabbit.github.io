b99a5b1438db914a447029d1586f0c8fb4417f6b
Kenneth.J <kenjones@163.com>
2014-07-28 09:45:35 +0800

    site 0.9.2

---
+++ js/baidu_tongji.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F4595de807ca83dbe251c95fcd9563422' type='text/javascript'%3E%3C/script%3E"));
